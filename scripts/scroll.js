bfdec9ce8df795235af3673c477a4288ab6f4f08
rustam-mansurov <otum@bk.ru>
2025-04-10 09:54:15 +0400

    new stuff

---
+++ scripts/scroll.js
@@ -0,0 +1,9 @@
+ // JS для плавной прокрутки (если нужно больше контроля)
+    document.querySelectorAll('.nav-link').forEach(link => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    });
